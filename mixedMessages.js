c482f328f4e867ed72efc8e5133680bc48a62be9
m2ygmmm <atmanim1@outlook.com>
2024-01-09 14:12:23 +0000

    version one

---
+++ mixedMessages.js
@@ -0,0 +1,11 @@
+/* Output a random event that will happen to you during the week,
+The random event will have a random day, random time and random activity 
+e.g., You will meet a famous celebrity at 18:00 on sunday*/
+
+//STEP ONE - Create an object containing arrays of days of the week and random events
+//STEP TWO - Create a function that will return a random time of day using the 24h clock
+//STEP THREE - Create a function that takes two arguments (Object, random time of day) an
+//by generating a random number to use as the index within the objects array and join it together with the random
+//time of day and and random time.
+//STEP FOUR - Output the final random event
+
